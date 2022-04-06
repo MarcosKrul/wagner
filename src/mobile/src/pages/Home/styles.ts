48adccb0fb56f4e51958c0b8c51368bf6bee96d1
Renato Ruppel <heruppel2@gmail.com>
2022-04-06 01:09:52 -0300

    added button mocks

--- src/mobile/src/pages/Home/styles.ts
+++ src/mobile/src/pages/Home/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { colors } from '../../global/colors';
 
 export const styles = StyleSheet.create({
@@ -10,10 +10,36 @@ export const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 20
+    padding: 15,
+    borderBottomWidth: 0.3,
+    borderBottomColor: '#000'
   },
   content: {
     flex: 18,
-    backgroundColor: colors.cgBlue
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  topButtons: {
+    width: Dimensions.get('window').width,
+    flex: 0.4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  middleButtons: {
+    width: Dimensions.get('window').width,
+    flex: 0.4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bottomButtons: {
+    width: Dimensions.get('window').width - 60,
+    flex: 0.4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  slider: {
+    width: Dimensions.get('window').width - 80,
+    height: 50,
+    flex: 0.3
   }
 })
